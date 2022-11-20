6d64f739fcde94d015696a17d98e2199b45871e6
Roman Bukharin <buxarin1999@gmail.com>
2022-11-20 23:05:23 +0600

    add modal

--- app/components/UI/PhoneInput.jsx
+++ app/components/UI/PhoneInput.jsx
@@ -2,7 +2,7 @@ import InputMask from 'react-input-mask';
 export default function PhoneInput({ className, placeholder}) {
     return (
       <>
-        <InputMask  mask='+7 (999) 999 9999'  className={`rounded-[10px] outline-none border formInputShadow pl-8 placeholder:text-lg placeholder:first-letter:text-black ${className}`} type="text" placeholder={placeholder} />
+        <InputMask  mask='+7 (999) 999 9999'  className={`rounded-[10px] outline-none border formInputShadow pl-8 placeholder:text-lg ${className}`} type="text" placeholder={placeholder} />
       </>
     );
   }
