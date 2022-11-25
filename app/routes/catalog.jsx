de6a8031a3079c47681342a4169660c507d8abf0
Roman Bukharin <buxarin1999@gmail.com>
2022-11-25 16:34:21 +0600

    add #22

--- app/routes/catalog.jsx
+++ app/routes/catalog.jsx
@@ -1,9 +1,19 @@
-import MainLayout from "../layouts/MainLayout";
-import CatalogPreviewCard from "../components/UI/CatalogPreviewCard";
+import MainLayout from '../layouts/MainLayout';
+import SendProject from '../components/SendProject';
+import MontageBlock from '../components/MontageBlock';
+import Consult from '../components/Consult';
+import Subscribe from '../components/Subscribe';
+import BoardDescription from '../components/BoardDescription';
+import CatalogHeader from '../components/UI/CatalogHeader';
 export default function Catalog() {
   return (
-  <MainLayout>
-    <CatalogPreviewCard title="Террасная доска" colors={['catalogColor.png','catalogColor.png','catalogColor.png','catalogColor.png','catalogColor.png',]}/>
-     </MainLayout>  
+    <MainLayout>
+      <CatalogHeader/>
+      <BoardDescription/>
+      <MontageBlock/>
+      <SendProject />
+      <Consult title='Получите консультацию' text={<><span className='font-bold'>по вашему объекту уже сегодня. </span> Оставьте заявку и наш менеджер <br/> свяжется с вами чтобы проконсультировать и рассчитать стоимость </>} />
+      <Subscribe/>
+    </MainLayout>
   );
 }
