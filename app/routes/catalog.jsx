fa7f667a274d3333a74fdf8861e95d3a3428e678
Roman Bukharin <buxarin1999@gmail.com>
2022-11-30 12:47:51 +0600

    add #27

--- app/routes/catalog.jsx
+++ app/routes/catalog.jsx
@@ -9,13 +9,12 @@ import CatalogFilter from '../components/CatalogFilter'
 export default function Catalog() {
   return (
     <CatalogLayout>
-      <CatalogFilter/>
-      {/* <CatalogProducts/>
+      <CatalogProducts/>
       <BoardDescription/>
       <MontageBlock/>
       <SendProject />
       <Consult title='Получите консультацию' text={<><span className='font-bold'>по вашему объекту уже сегодня. </span> Оставьте заявку и наш менеджер <br/> свяжется с вами чтобы проконсультировать и рассчитать стоимость </>} />
-      <Subscribe/> */}
+      <Subscribe/>
     </CatalogLayout>
   );
 }
