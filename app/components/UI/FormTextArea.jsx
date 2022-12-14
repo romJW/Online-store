78a9c33aa49de949dc713b5623c2742a9985411b
Roman Bukharin <buxarin1999@gmail.com>
2022-12-14 14:36:37 +0600

    add dev

--- app/components/UI/FormTextArea.jsx
+++ app/components/UI/FormTextArea.jsx
@@ -1,7 +1,7 @@
 export default function FormInput({ className, placeholder}) {
     return (
       <>
-        <textarea  className={`outline-none border formInputShadow resize-none pt-7 pl-8 rounded-[10px] shadow-xl pl-8 placeholder:text-lg pr-4 ${className}`} type="text" placeholder={placeholder} />
+        <textarea  className={`outline-none border formInputShadow resize-none pt-7 rounded-[10px] shadow-xl pl-8 placeholder:text-lg pr-4 ${className}`} type="text" placeholder={placeholder} />
       </>
     );
   }
